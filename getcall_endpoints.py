059540a9b1f6e30a26a342da3ea50f18117a909f
whitehat92 <ze_correia93@hotmail.com>
2020-02-05 14:12:03 +0000

    request to url and find endpoints

---
+++ getcall_endpoints.py
@@ -0,0 +1,26 @@
+import requests
+import sys
+from urllib3.exceptions import InsecureRequestWarning
+import re
+
+requests.urllib3.disable_warnings()
+
+
+
+endpointsregex = "(\/\w*)?(\w+\/)"
+newendpointsregex = "(\/\w*)+(\d*)"
+url = sys.argv[1]
+if not "https://" in url:
+        url = "https://" + url
+headers={'User-agent':'Mozilla//5.0',}
+r = requests.get(url=url, verify=False,headers=headers,timeout=2)
+getcontent = r.content.decode("utf-8", "ignore") #get the entire content of website if successful, has to be ISO-8859-1 to decode the content
+#decodedcontent = getcontent.decode("uft-8") if utf-8 it doesn't work as it can't decode an unspecified character
+endpointsfind = re.findall(endpointsregex,getcontent,re.DOTALL) #this is to get a list
+
+for endpointspresent in re.finditer(newendpointsregex, getcontent): #this already prints as an iteration like the usual
+        print(endpointspresent.group())
+
+
+
+
